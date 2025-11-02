5cf7693d7af2481f50881a501ff1ccda4b8a7d80
Pabluntt <pablo.lopez.saavedra3001@gmail.com>
2025-11-02 20:12:59 -0300

    CAmbio color y nombre y tailwind
    le cambie los colores y lepuse TAILWIND

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import ProductCard from '../components/ProductCard'
 export default function Home() {
   return (
     <div>
-      <h1>Tienda demo</h1>
+      <h1 className='font-bold text-2xl'>Tienda demo</h1>
       <div className="grid">
         {products.map(p => (
           <ProductCard key={p.id} product={p} />
